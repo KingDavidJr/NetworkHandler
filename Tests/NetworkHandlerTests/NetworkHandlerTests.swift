e4929b29cfc9ceb167eb66ebb929c948be6edaf2
David <realkd872@gmail.com>
2025-07-06 23:49:42 -0700

    Created Tests for the Network Handler

--- Tests/NetworkHandlerTests/NetworkHandlerTests.swift
+++ Tests/NetworkHandlerTests/NetworkHandlerTests.swift
@@ -1,6 +1,52 @@
 import Testing
+import Foundation
 @testable import NetworkHandler
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//MARK: Setup TestNetworkHandler Environment
+
+let mockNetworkHandler = NetworkHandler()
+
+// Test this too
+class NetworkHandlerMockURL: NetworkHandlerURLProtocol {
+    var url: URL?
+    var urlAsString: String?
+    init(url: URL) {
+        self.url = url
+    }
+    init (url: String) {
+        self.urlAsString = url
+    }
+    
+    func convertStringToURL() -> URL? {
+        if let urlAsString = urlAsString {
+            return URL(string: urlAsString)
+        }
+        return nil
+    }
+}
+
+@Suite("Test GET Requests")
+struct TestNetworkHandlerFetchMethods {
+    
+    let networkHandlerMockURL = NetworkHandlerMockURL(url: "https://postman-echo.com/get").convertStringToURL()
+    
+    @Test("Test Fetch Data")
+    func testFetchData() async throws {
+        if let networkHandlerMockURL {
+            do {
+                let data = try await mockNetworkHandler.fetchData(from: networkHandlerMockURL)
+                if let data = data {
+                    print(data)
+                }
+            }
+        }
+        #expect("Test" == "Test") // FIX THIS
+    }
+    
+    @Test("Test Fetch Data with invalid URL")
+    func testFetchDataWithInvalidURL() async throws {
+        let _ = await #expect(throws: NetworkHandler.NetworkError.self) {
+            try await mockNetworkHandler.fetchData(from: URL(filePath: "postman-echo.com/get")!)
+        }
+    }
 }
