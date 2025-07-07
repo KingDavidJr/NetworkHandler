2a1e1b7c2e51d67000e12055a0fe3d91752e049d
David <realkd872@gmail.com>
2025-07-06 23:57:41 -0700

    Changed name to align with others

--- Sources/NetworkHandler/NetworkHandlerProtocol.swift
+++ Sources/NetworkHandler/NetworkHandlerProtocol.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public protocol NetworkManagerProtocol {
+public protocol NetworkHandlerProtocol {
     @available(macOS 12.0, *) func fetchData(from url: URL, with headers: [String: String]) async throws -> Data?
     func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T
     func encode<T: Encodable>(_ value: T) throws -> Data
