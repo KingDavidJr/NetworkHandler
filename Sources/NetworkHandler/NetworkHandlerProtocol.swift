1be58987efa502090ce24f84b0d26f4012ddca7c
David <realkd872@gmail.com>
2025-07-06 23:49:01 -0700

    init protocol

---
+++ Sources/NetworkHandler/NetworkHandlerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkHandlerProtocol.swift
+//  NetworkHandler
+//
+//  Created by David Amedeka on 7/6/25.
+//
+
+import Foundation
+
+protocol NetworkManagerProtocol {
+    @available(macOS 12.0, *) func fetchData(from url: URL, with headers: [String: String]) async throws -> Data?
+    func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T
+    func encode<T: Encodable>(_ value: T) throws -> Data
+}
