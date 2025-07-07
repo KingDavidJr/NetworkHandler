4646a749b32990153503b01f8488192917fd6c0d
David <realkd872@gmail.com>
2025-07-06 23:49:28 -0700

    Created NetworkHandler

--- Sources/NetworkHandler/NetworkHandler.swift
+++ Sources/NetworkHandler/NetworkHandler.swift
@@ -1,2 +1,62 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public final class NetworkHandler: NetworkManagerProtocol, Sendable {
+    enum NetworkError: Error {
+        case invalidURL
+        case invalidResponse
+        case decodingError(Error)
+        case httpError(statusCode: Int, response: URLResponse)
+        case requestFailed(Error)
+        case otherError(Error)
+    }
+    
+    @available(macOS 12.0, *)
+    func fetchData(from url: URL, with headers: [String: String] = [:]) async throws -> Data? {
+        do {
+            guard url != URL("") else {
+                throw NetworkError.invalidURL
+            }
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            if headers != [:] {
+                for (key, value) in headers {
+                    request.addValue(value, forHTTPHeaderField: key)
+                }
+            }
+            let (data, response) = try await URLSession.shared.data(for: request)
+            if let httpResponse = response as? HTTPURLResponse {
+                guard (200...299).contains(httpResponse.statusCode) else {
+                    throw NetworkError.httpError(statusCode: httpResponse.statusCode, response: response)
+                }
+                return data
+            }
+        } catch let urlError as URLError {
+            throw NetworkError.requestFailed(urlError)
+        } catch  {
+            throw NetworkError.requestFailed(error)
+        }
+        return nil
+    }
+    
+    func decode<T: Decodable>(_ type: T.Type, from data: Data) throws -> T {
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+    
+    func encode<T>(_ value: T) throws -> Data where T : Encodable {
+        return try JSONEncoder().encode(value)
+    }
+}
+
+extension NetworkHandler {
+    @available(macOS 12.0, *)
+    func fetchDataAndDecode<T: Decodable>(from url: URL, as type: T.Type) async throws -> T {
+        guard let data = try await fetchData(from: url) else {
+            throw NetworkError.otherError(NSError(domain: "NetworkHandler", code: 1001, userInfo: [NSLocalizedDescriptionKey : "No data returned from URL"]))
+        }
+        return try decode(type, from: data)
+    }
+}
